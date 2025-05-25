8c3f0a1e12075a39e1ec9c2f60751f118185589d
Myrna <myrnaz@nycstudents.net>
2025-05-24 20:02:19 -0400

    Add the frame and displayPanel...(copied the goomba code)

--- src/Frame.java
+++ src/Frame.java
@@ -1,2 +1,16 @@
+import javax.swing.*;
+
 public class Frame {
+    public Frame() {
+        JFrame frame = new JFrame("Frame title");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 700);
+        frame.setLocationRelativeTo(null);
+
+        DisplayPanel panel = new DisplayPanel();
+        frame.add(panel);
+
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
 }

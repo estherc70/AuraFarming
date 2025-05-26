19562b674329fddcd98f4f7f9dc1f38b7bf41136
Nikki <nikkizhang320@gmail.com>
2025-05-25 23:07:58 -0400

    added cardLayout to Frame class, added PanelClass and ButtonClass for organization

--- src/Frame.java
+++ src/Frame.java
@@ -1,16 +1,47 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Frame {
+public class Frame extends JFrame{
+    private JPanel mainPanel;
+    private ButtonClass buttonClass;
+    private CardLayout cardLayout;
+
     public Frame() {
-        JFrame frame = new JFrame("Frame title");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1000, 700);
-        frame.setLocationRelativeTo(null);
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+        buttonClass = new ButtonClass(this);
 
-        DisplayPanel panel = new DisplayPanel();
-        frame.add(panel);
+        JPanel startScreen = PanelClass.createPanel("src/images/start.png");
 
-        frame.setResizable(false);
-        frame.setVisible(true);
+        //manually control button positions
+        startScreen.setLayout(null);
+
+        startScreen.add(buttonClass.getStartbtn());
+
+        mainPanel.add(startScreen, "StartScreen");
+
+        Container pane = getContentPane();
+        pane.add(mainPanel, BorderLayout.CENTER);
+        pane.add(buttonClass.getBtnPanel(), BorderLayout.SOUTH);
+
+        setTitle("CardLayoutOrganized");
+        setSize(1000, 700); // Set the size of the window
+        setResizable(false); // Disable resizing the window
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); // Center the window on the screen
+        setVisible(true);
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    //switch through card panels ((used in ButtonPanel for ActionListeners)
+    public void showCard(String cardName) {
+        cardLayout.show(mainPanel, cardName);
+    }
+
+    public static Frame createFrame() {
+        return new Frame();
     }
 }

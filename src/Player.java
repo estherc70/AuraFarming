0ce1742ca4445b16b4ca90e873757893d0d824f3
Nikki <nikkizhang320@gmail.com>
2025-05-26 01:16:59 -0400

    added a username text field and custom cursor

---
+++ src/Player.java
@@ -0,0 +1,18 @@
+public class Player {
+    private String username;
+    private int followers;
+
+    public Player() {
+        username = "";
+        followers = 0;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String newUser) {
+        username = newUser;
+    }
+
+}

0102f8c228abfa510998f96b3ac13cfc88811aeb
Nikki <nikkizhang320@gmail.com>
2025-06-03 00:05:35 -0400

    added the logic for tictactoe

--- src/Livestream.java
+++ src/Livestream.java
@@ -3,19 +3,12 @@ import java.util.ArrayList;
 public class Livestream {
     private String[] goodChat = {"first time here!", "whoa, sick set up", "cool keyboard", "what is this stream about?",
     "you got yourself a new follower!", "im so early lolol", "can you play a horror game?", "i gtg now have a good stream!", "can i get a hi",
-    "what did you eat today", "can we see your cat"};
+    "what did you eat today", "can we see your cat", "love this bro", "youre my favorite streamer"};
     private String[] badChat = {"LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL", "this dude sucks LOL", "disahdiosahdioahdiaohdaios", "LOSER",
-    "HAHAHAHAHAHAHAH", "stinky streamer", "BOOOO", "follow me instead of this guy", "your ugly", "stop streaming", "reportreportreport"};
-    private String[] currentChat;
-    private int chatNum;
+    "HAHAHAHAHAHAHAH", "stinky streamer", "BOOOO", "follow me instead of this guy", "your ugly", "stop streaming", "reportreportreport", "yo mama",
+    };
 
     public Livestream() {
-        currentChat = new String[10];
-        chatNum = 0;
-    }
-
-    public boolean checkInBounds() {
-        return chatNum < currentChat.length;
     }
 
     public String getRandomGood() {
@@ -26,33 +19,5 @@ public class Livestream {
     public String getRandomBad() {
         int random = (int) (Math.random() * badChat.length - 1);
         return badChat[random];
-    }
-
-    public void moveChatUp() {
-        for (int i = 0; i < currentChat.length - 1; i++) {
-            String temp = currentChat[i+2];
-            currentChat[i] = currentChat[i+1];
-            currentChat[i+1] = temp;
-        }
-    }
-
-    public String[] getCurrentChat() {
-        return currentChat;
-    }
-
-    public int getChatNum() {
-        return chatNum;
-    }
-
-    public void setChatNum(int num) {
-        chatNum = num;
-    }
-
-    public void incrementChatNum() {
-        chatNum++;
-    }
-
-    public void setCurrentChat(int index, String str) {
-        currentChat[index] = str;
     }
 }
